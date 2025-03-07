019a3c6f205be69718b459a8dbe8d4226a2ed2c7
Mohammad karshnas <mkarshnas6@gmail.com>
2025-03-08 02:51:18 +0330

    create all animation and add loading for animation saveing and end

--- app/src/main/java/com/mkarshnas6/karenstudio/karenbalance/SavingFragment.kt
+++ app/src/main/java/com/mkarshnas6/karenstudio/karenbalance/SavingFragment.kt
@@ -1,20 +1,18 @@
 package com.mkarshnas6.karenstudio.karenbalance
 
 import android.annotation.SuppressLint
-import android.graphics.drawable.LayerDrawable
-import android.graphics.drawable.ShapeDrawable
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
-import app.rive.runtime.kotlin.core.Direction
-import app.rive.runtime.kotlin.core.Loop
 import app.rive.runtime.kotlin.core.Rive
 import com.mkarshnas6.karenstudio.karenbalance.databinding.ActivitySavingBinding
+
 
 class SavingFragment : Fragment(R.layout.activity_saving) {
 
@@ -33,7 +31,6 @@ class SavingFragment : Fragment(R.layout.activity_saving) {
 
         Rive.init(requireContext())
 
-        binding.txtSaveDaily.visibility = View.INVISIBLE
 
 //        show animation write save today and monthly ............
         AnimationWrite.showAnimTitle(binding.txtSaveDaily, "Amount saved today")
@@ -41,9 +38,15 @@ class SavingFragment : Fragment(R.layout.activity_saving) {
 
 //      ...........set value state matchins ........................
         Handler(Looper.getMainLooper()).postDelayed({
+            binding.progressBarDaily.visibility = View.INVISIBLE
+            binding.progressBarMontly.visibility = View.INVISIBLE
+
+            binding.animSavingDaily.visibility = View.VISIBLE
+            binding.animSavingMonthly.visibility = View.VISIBLE
+
             binding.animSavingDaily.setNumberState("anim saving", "save", 100f)
             binding.animSavingMonthly.setNumberState("anim saving", "save", 10f)
-        }, 3000)
+        }, 1000)
 
         return binding.root
     }

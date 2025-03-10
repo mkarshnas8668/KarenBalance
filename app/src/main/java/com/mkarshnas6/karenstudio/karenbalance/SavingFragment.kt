94caf566c1eca3d11cd99673f2c075acc0d8933f
Mohammad karshnas <mkarshnas6@gmail.com>
2025-03-10 08:20:32 +0330

    add file animation and loading for animation

--- app/src/main/java/com/mkarshnas6/karenstudio/karenbalance/SavingFragment.kt
+++ app/src/main/java/com/mkarshnas6/karenstudio/karenbalance/SavingFragment.kt
@@ -1,54 +1,82 @@
 package com.mkarshnas6.karenstudio.karenbalance
 
 import android.annotation.SuppressLint
-import android.graphics.PorterDuff
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
 import androidx.fragment.app.Fragment
 import app.rive.runtime.kotlin.core.Rive
 import com.mkarshnas6.karenstudio.karenbalance.databinding.ActivitySavingBinding
-
+import com.mkarshnas6.karenstudio.karenbalance.db.DBHandler
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class SavingFragment : Fragment(R.layout.activity_saving) {
 
     private lateinit var binding: ActivitySavingBinding
-//    @OptIn(ExperimentalAssetLoader::class)
-//    private lateinit var animation_daily: RiveAnimationView
+    private var totalExpenseToday = 0
+    private var totalExpenseMonthly = 0
+    private var dailySpendingLimit = 0
+    lateinit var pref: SharedPreferences
 
-    //    @OptIn(ExperimentalAssetLoader::class)
-    @SuppressLint("UseCompatLoadingForDrawables")
+    @SuppressLint("UseCompatLoadingForDrawables", "CheckResult", "SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = ActivitySavingBinding.inflate(inflater)
-
         Rive.init(requireContext())
 
+        pref = requireContext().getSharedPreferences("Prefs_KarenBalance", MODE_PRIVATE)
+        val monthly_income = pref.getLong("monthly_income", 1111111111)
+        dailySpendingLimit = (monthly_income / 31).toInt()
 
-//        show animation write save today and monthly ............
-        AnimationWrite.showAnimTitle(binding.txtSaveDaily, "Amount saved today")
-        AnimationWrite.showAnimTitle(binding.txtSaveMonthly, "Amount saved this month")
+//        show limie prescent daily .......................
+        val db = DBHandler.getDatabase(requireContext())
+        db.dailyDao().getDailys
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ expenses ->
 
-//      ...........set value state matchins ........................
+                totalExpenseToday = expenses.sumOf { it.price }
+                val totalExpenseTodayPercent = (totalExpenseToday.toFloat() / dailySpendingLimit) * 100
+
+                binding.txtSaveDaily.text = "${getString(R.string.amount_saved_today)} :\n ${100-totalExpenseTodayPercent.toInt()}%"
+                binding.animSavingDaily.setNumberState("anim saving", "expen", totalExpenseTodayPercent)
+                AnimationWrite.showAnimTitle(binding.txtSaveDaily, binding.txtSaveDaily.text.toString())
+
+            }, { error -> error.printStackTrace() })
+
+        //        show limie prescent monthly .......................
+        db.reportDao().getReports
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ expenses ->
+
+                totalExpenseMonthly = expenses.sumOf { it.price }
+                val totalExpenseMonthlyPercent = (totalExpenseMonthly.toFloat() / monthly_income) * 100
+
+                binding.txtSaveMonthly.text = "${getString(R.string.amount_saved_this_month)} :\n ${100-totalExpenseMonthlyPercent.toInt()}%"
+                binding.animSavingMonthly.setNumberState("anim saving", "expen", totalExpenseMonthlyPercent)
+                AnimationWrite.showAnimTitle(binding.txtSaveMonthly, binding.txtSaveMonthly.text.toString())
+
+            }, { error -> error.printStackTrace() })
+
+
         Handler(Looper.getMainLooper()).postDelayed({
             binding.progressBarDaily.visibility = View.INVISIBLE
             binding.progressBarMontly.visibility = View.INVISIBLE
-
             binding.animSavingDaily.visibility = View.VISIBLE
             binding.animSavingMonthly.visibility = View.VISIBLE
-
-            binding.animSavingDaily.setNumberState("anim saving", "save", 100f)
-            binding.animSavingMonthly.setNumberState("anim saving", "save", 10f)
+            binding.animSavingMonthly.setNumberState("anim saving", "expen", 10f)
         }, 1000)
 
         return binding.root
     }
-
 }
